6ae13e846374424f2d9d0e202d3338cc85456ea2
Alexander <sany199574@gmail.com>
2024-06-05 13:59:23 +0300

    Добавлен алгоритм, на поиск наименьшего отсутствуещего возможного целого числа

--- project_41_FirstMissingPositive/Program.cs
+++ project_41_FirstMissingPositive/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int[] input1 = { 1, 2, 0 };
+int[] input2 = { 3, 4, -1, 1 };
+int[] input3 = { 7, 8, 9, 11, 12 };
+
+Console.WriteLine(FirstMissingPositive(input1));
+Console.WriteLine(FirstMissingPositive(input2));
+Console.WriteLine(FirstMissingPositive(input3));
+
+Console.ReadLine();
+
+
+int FirstMissingPositive(int[] nums)
+{
+    int digit = 1;
+    Array.Sort(nums);
+    
+
+    for (int i = 0; i < nums.Length; i++)
+	{
+		if (nums[i] <= 0) { continue; }
+
+        if(nums[i] == digit) {  digit++; }
+	}
+    return digit;
+}
