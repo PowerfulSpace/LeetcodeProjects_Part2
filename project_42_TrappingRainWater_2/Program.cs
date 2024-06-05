15e098b101c5ef317b1697cb47172ef443e4336f
Alexander <sany199574@gmail.com>
2024-06-05 15:57:05 +0300

    Добавлена новая задачкааа

---
+++ project_42_TrappingRainWater_2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+int[] height1 = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+int[] height2 = { 4, 2, 0, 3, 2, 5 };
+
+
+Console.WriteLine(Trap(height1));
+Console.WriteLine(Trap(height2));
+
+Console.ReadLine();
+
+
+int Trap(int[] height)
+{
+    return 1;
+}
+
