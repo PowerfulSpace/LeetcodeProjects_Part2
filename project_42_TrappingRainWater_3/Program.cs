c1299d7881ed79c878b6943a91163a9773062d43
Alexander <sany199574@gmail.com>
2024-06-07 19:23:45 +0300

    Добавлен алгоритм

---
+++ project_42_TrappingRainWater_3/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+
+int[] height1 = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+int[] height2 = { 4, 2, 0, 3, 2, 5 };
+int[] height3 = { 5, 1, 2, 0, 11, 9, 1, 1, 3, 3, 5, 6, 3, 2, 1, 5, 5, 1, 1, 1, 5, 0, 1, 3, 8, 6, 4 };
+int[] height4 = { 2, 0, 2 };
+int[] height5 = { 0, 1, 2, 0, 3, 0, 1, 2, 0, 0, 4, 2, 1, 2, 5, 0, 1, 2, 0, 2 };
+int[] height6 = { 5, 4, 1, 2 };
+int[] height7 = { 9, 6, 8, 8, 5, 6, 3 };
+
+
+//Console.WriteLine(Trap(height1));
+//Console.WriteLine(Trap(height2));
+Console.WriteLine(Trap(height3));
+//Console.WriteLine(Trap(height4));
+//Console.WriteLine(Trap(height5));
+//Console.WriteLine(Trap(height6));
+//Console.WriteLine(Trap(height7));
+
+Console.ReadLine();
+
+
+int Trap(int[] height)
+{
+    int trappedWater = 0;
+    int leftMax = height[0];
+    int rightMax = height[height.Length - 1];
+
+    int l = 0, r = height.Length - 1;
+
+    while (l < r)
+    {
+        if (height[l] <= height[r])
+        {
+            if (leftMax < height[l])
+            {
+                leftMax = height[l];
+            }
+            else
+            {
+                trappedWater += leftMax - height[l];
+            }
+            l++;
+        }
+        else
+        {
+            if (rightMax < height[r])
+            {
+                rightMax = height[r];
+            }
+            else
+            {
+                trappedWater += rightMax - height[r];
+            }
+            r--;
+        }
+    }
+
+    return trappedWater;
+}
