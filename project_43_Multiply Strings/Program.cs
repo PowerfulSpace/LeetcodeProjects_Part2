96e524f01180f38db7798c7298e07a52f928a03e
Alexander <sany199574@gmail.com>
2024-06-08 00:43:09 +0300

    Добавлен алгоритм

--- project_43_Multiply Strings/Program.cs
+++ project_43_Multiply Strings/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 
+using System.Text;
+
 string numA1 = "2";
 string numA2 = "3";
 
@@ -10,17 +12,36 @@ string numB2 = "456";
 string numC1 = "9333852702227987";
 string numC2 = "85731737104263";
 
-//Console.WriteLine(Multiply(numA1, numA2));
+Console.WriteLine(Multiply(numA1, numA2));
 //Console.WriteLine(Multiply(numB1, numB2));
-Console.WriteLine(Multiply(numC1, numC2));
+//Console.WriteLine(Multiply(numC1, numC2));
 
 Console.ReadLine();
 
 string Multiply(string num1, string num2)
 {
-    decimal a = decimal.Parse(num1);
-    decimal b = decimal.Parse(num2);
+    string a = "346";
+    string b = "843";
 
-    string result = Convert.ToString(a * b);
-    return result;
+    int indexA = a.Length - 1;
+    int indexB = b.Length - 1;
+
+    StringBuilder sb = new StringBuilder(a);
+
+    for (int i = 0; i < Math.Max(a.Length,b.Length); i++)
+    {
+        if(indexA > 0 && indexB > 0)
+        {
+            int a1 = Convert.ToInt32(char.ConvertFromUtf32(a[indexA]));
+            int b1 = Convert.ToInt32(char.ConvertFromUtf32(b[indexB]));
+
+            int s = a1 * b1;
+
+            indexA--;
+            indexB--;
+        }
+    }
+
+
+    return "1";
 }
