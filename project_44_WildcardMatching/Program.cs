cc53815162161ad336ec3fb4cafbe9bcf48cd4a0
Alexander <sany199574@gmail.com>
2024-06-10 05:26:16 +0300

    Добавлен проект, реализация своего регулярного выражения

--- project_44_WildcardMatching/Program.cs
+++ project_44_WildcardMatching/Program.cs
@@ -1,12 +1,25 @@
 ﻿
-
+using System.Text.RegularExpressions;
 
 string s1 = "aa";
 string p1 = "a";
+
+string s2 = "aa";
+string p2 = "*";
+
+string s3 = "cb";
+string p3 = "?a";
+
+Console.WriteLine(IsMatch(s1,p1));
+Console.WriteLine(IsMatch(s2,p2));
+Console.WriteLine(IsMatch(s3,p3));
 
 Console.ReadLine();
 
 bool IsMatch(string s, string p)
 {
-    return;
+
+    var result = Regex.IsMatch(s, p);
+
+    return result;
 }
