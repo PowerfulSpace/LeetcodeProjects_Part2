ddd6d019fc8b05fc412b3a526b4bd19552dad28e
Alexander <sany199574@gmail.com>
2024-06-11 03:42:20 +0300

    Добавлен проект, альтетнативный вариант предыдущему проекту

---
+++ project_44_WildcardMatching_2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Text;
+
+string s1 = "aa";
+string p1 = "a";
+
+string s2 = "aa";
+string p2 = "*";
+
+string s3 = "cb";
+string p3 = "?a";
+
+string s4 = "acdcb";
+string p4 = "a*c?b";
+
+string s5 = "adceb";
+string p5 = "*a*b";
+
+string s6 = "";
+string p6 = "*****";
+
+string s7 = "a";
+string p7 = "aa";
+
+string s8 = "abcabczzzde";
+string p8 = "*abc???de*";
+
+string s9 = "aab";
+string p9 = "c*a*b";
+
+string s10 = "abefcdgiescdfimde";
+string p10 = "ab*cd?i*de";
+
+//Console.WriteLine(IsMatch(s1, p1));
+//Console.WriteLine(IsMatch(s2, p2));
+//Console.WriteLine(IsMatch(s3, p3));
+//Console.WriteLine(IsMatch(s4, p4));
+//Console.WriteLine(IsMatch(s5, p5));
+//Console.WriteLine(IsMatch(s6, p6));
+//Console.WriteLine(IsMatch(s7, p7));
+//Console.WriteLine(IsMatch(s8, p8));
+//Console.WriteLine(IsMatch(s9, p9));
+Console.WriteLine(IsMatch(s10, p10));
+
+Console.ReadLine();
+
+bool IsMatch(string s, string p)
+{
+
+    //Реализовать проект с помощи рекурсии
+
+    return true;
+}
