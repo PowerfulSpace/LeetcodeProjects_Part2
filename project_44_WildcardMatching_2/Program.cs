dddb614af49d3691503499de06cf9cd3938edfe6
Alexander <sany199574@gmail.com>
2024-06-12 04:29:07 +0300

    Переписан алгоритм

--- project_44_WildcardMatching_2/Program.cs
+++ project_44_WildcardMatching_2/Program.cs
@@ -1,7 +1,4 @@
 ﻿
-using System;
-using System.Text;
-
 string s1 = "aa";
 string p1 = "a";
 
@@ -32,13 +29,13 @@ string p9 = "c*a*b";
 string s10 = "abefcdgiescdfimde";
 string p10 = "ab*cd?i*de";
 
-Console.WriteLine(IsMatch(s1, p1));
-Console.WriteLine(IsMatch(s2, p2));
-Console.WriteLine(IsMatch(s3, p3));
-Console.WriteLine(IsMatch(s4, p4));
-Console.WriteLine(IsMatch(s5, p5));
-Console.WriteLine(IsMatch(s6, p6));
-Console.WriteLine(IsMatch(s7, p7));
+//Console.WriteLine(IsMatch(s1, p1));
+//Console.WriteLine(IsMatch(s2, p2));
+//Console.WriteLine(IsMatch(s3, p3));
+//Console.WriteLine(IsMatch(s4, p4));
+//Console.WriteLine(IsMatch(s5, p5));
+//Console.WriteLine(IsMatch(s6, p6));
+//Console.WriteLine(IsMatch(s7, p7));
 Console.WriteLine(IsMatch(s8, p8));
 Console.WriteLine(IsMatch(s9, p9));
 Console.WriteLine(IsMatch(s10, p10));
@@ -47,8 +44,34 @@ Console.ReadLine();
 
 bool IsMatch(string s, string p)
 {
+    List<char> chars = new List<char>(s);
 
-    //Реализовать проект с помощи рекурсии
+    Examination(chars, p, 0,0);
 
-    return true;
+    chars.RemoveAll(x => x == '-');
+
+    bool result = chars.Count == 0 ? true : false;
+
+    return result;
+}
+
+void Examination(List<char> s, string p, int indexS, int indexP)
+{
+    if (indexS >= s.Count || indexP >= p.Length) { return; }
+
+    if (p[indexP] == s[indexS] || p[indexP] == '?')
+    {
+        Examination(s, p, indexS + 1, indexP + 1);
+        s[indexS] = '-';
+    }
+    else if (p[indexP] == '*')
+    {      
+        if (indexP < p.Length && p[indexP + 1] == s[indexS])
+        {
+            indexP+=2;
+        }
+        Examination(s, p, indexS+1, indexP);
+        s[indexS] = '-';
+    }
+
 }
