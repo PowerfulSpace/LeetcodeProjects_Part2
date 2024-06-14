063d704040a385c7f28c2b9576cffea669dcf249
Alexander <sany199574@gmail.com>
2024-06-14 15:38:57 +0300

    Переписал алгоритм

--- project_45_Jump Game II/Program.cs
+++ project_45_Jump Game II/Program.cs
@@ -6,6 +6,7 @@ int[] nums3 = { 0};
 int[] nums4 = { 1, 1, 1, 1 };
 int[] nums5 = { 2,1 };
 int[] nums6 = { 1, 2, 1, 1, 1 };
+int[] nums7 = { 4, 1, 1, 3, 1, 1, 1 };
 
 
 Console.WriteLine(Jump(nums1));
@@ -14,6 +15,7 @@ Console.WriteLine(Jump(nums3));
 Console.WriteLine(Jump(nums4));
 Console.WriteLine(Jump(nums5));
 Console.WriteLine(Jump(nums6));
+Console.WriteLine(Jump(nums7));
 
 Console.ReadLine();
 
@@ -22,13 +24,45 @@ int Jump(int[] nums)
 {
 	if(nums.Length <= 1) return 0;
 
-	int count = 0;
+	int maxStep = nums[0];
+	int step = nums[0];
 	int index = 0;
+	int count = 0;
 
-	while (index < nums.Length &&  nums[index] != 0)
+	for(int i = 0; i < nums.Length; i++)
 	{
-		index += nums[index] + 1;
-        count++;
+		if(maxStep >= nums.Length - 1)
+		{
+			count++;
+			break;
+		}
+		if (i < maxStep )
+		{
+			if (nums[i] + step > step)
+			{
+				step = nums[i];
+				index = i;
+            }
+		}
+		else if(i == maxStep)
+		{
+            if(maxStep < step + nums[index])
+			{
+				maxStep = step + index;
+                i = index;
+            }
+			else
+			{
+				maxStep = nums[i] + maxStep;
+
+            }
+			if (nums[i] == 0) { break; }
+
+			if(i < nums.Length)
+			{
+                count++;
+            }
+        }
     }
 
     return count; 
