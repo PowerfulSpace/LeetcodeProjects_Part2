d6e59bfe8a18ac162521bde01721458ba3389169
Alexander <sany199574@gmail.com>
2024-06-14 12:16:28 +0300

    Добавлен новый проект

---
+++ project_45_Jump Game II/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+int[] nums1 = { 2, 3, 1, 1, 4 };
+int[] nums2 = { 2, 3, 0, 1, 4 };
+int[] nums3 = { 0};
+int[] nums4 = { 1, 1, 1, 1 };
+int[] nums5 = { 2,1 };
+int[] nums6 = { 1, 2, 1, 1, 1 };
+
+
+Console.WriteLine(Jump(nums1));
+Console.WriteLine(Jump(nums2));
+Console.WriteLine(Jump(nums3));
+Console.WriteLine(Jump(nums4));
+Console.WriteLine(Jump(nums5));
+Console.WriteLine(Jump(nums6));
+
+Console.ReadLine();
+
+
+int Jump(int[] nums)
+{
+	if(nums.Length <= 1) return 0;
+
+	int count = 0;
+	int index = 0;
+
+	while (index < nums.Length &&  nums[index] != 0)
+	{
+		index += nums[index] + 1;
+        count++;
+    }
+
+    return count; 
+}
+
