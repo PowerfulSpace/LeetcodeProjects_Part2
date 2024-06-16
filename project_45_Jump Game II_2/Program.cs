f2044c29143300763a6de106ed42a56c67da6643
Alexander <sany199574@gmail.com>
2024-06-16 14:51:28 +0300

    Переписан алгоритм

--- project_45_Jump Game II_2/Program.cs
+++ project_45_Jump Game II_2/Program.cs
@@ -8,61 +8,62 @@ int[] nums4 = { 1, 1, 1, 1 };
 int[] nums5 = { 2, 1 };
 int[] nums6 = { 1, 2, 1, 1, 1 };
 int[] nums7 = { 4, 1, 1, 3, 1, 1, 1 };
-int[] nums8 = { 3,2, 1 };
+int[] nums8 = { 3, 2, 1 };
 int[] nums9 = { 3, 4, 3, 2, 5, 4, 3 };
 int[] nums10 = { 1, 2, 3, 4, 5 };
+int[] nums11 = { 1,0,0,0,0,5 };
 
 
-Console.WriteLine(Jump(nums1));
-Console.WriteLine(Jump(nums2));
-Console.WriteLine(Jump(nums3));
-Console.WriteLine(Jump(nums4));
-Console.WriteLine(Jump(nums5));
-Console.WriteLine(Jump(nums6));
-Console.WriteLine(Jump(nums7));
-Console.WriteLine(Jump(nums8));
-Console.WriteLine(Jump(nums9));
-Console.WriteLine(Jump(nums10));
+//Console.WriteLine(Jump(nums1));
+//Console.WriteLine(Jump(nums2));
+//Console.WriteLine(Jump(nums3));
+//Console.WriteLine(Jump(nums4));
+//Console.WriteLine(Jump(nums5));
+//Console.WriteLine(Jump(nums6));
+//Console.WriteLine(Jump(nums7));
+//Console.WriteLine(Jump(nums8));
+//Console.WriteLine(Jump(nums9));
+//Console.WriteLine(Jump(nums10));
+Console.WriteLine(Jump(nums11));
 
 Console.ReadLine();
 
 
 int Jump(int[] nums)
 {
-    if (nums.Length <= 1 || nums[0] == 0) return 0;
+    if (nums.Length <= 1 || nums[0] == 0) { return 0; }
+
+    int step = 1;
 
     int currentIndex = 0;
-    int endIndex = nums.Length - 1;
+    int lastIndex = nums.Length - 1;
 
-    int count = 1;
 
-    while (currentIndex < endIndex)
+    while (currentIndex < lastIndex)
     {
         int currentNum = nums[currentIndex];
 
-        if(currentNum + currentIndex >= endIndex) { return count; }
+        if(currentNum + currentIndex >= lastIndex) { return step; }
 
         int maxJump = 0;
         int nextIndex = -1;
 
-        for (int step = 1; step <= currentNum; step++)
+        for (int i = 1; i <= currentNum; i++)
         {
-            if (nums[currentIndex + step] + step > maxJump)
+            if (nums[currentIndex + i] + i >= maxJump)
             {
-                maxJump = nums[currentIndex + step] + step;
-                nextIndex = currentIndex + step;
+                maxJump = nums[currentIndex + i] + i;
+                nextIndex = currentIndex + i;
             }
         }
 
-        if(nextIndex == -1) { return 0; }
+        if (nextIndex == -1) { return 0; }
 
-        count++;
-        currentIndex = nextIndex;   
+        step++;
+        currentIndex = nextIndex;
     }
 
 
-
-   
-    return count;
+    return step;
 }
 
