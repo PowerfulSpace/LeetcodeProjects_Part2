925698ab121c29169c20220b288e2033ebd9fe8a
Alexander <sany199574@gmail.com>
2024-06-19 00:04:29 +0300

    Добавлена задача PermuteUnique

---
+++ project_47_Permutations II/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+int[] nums1 = { 1, 1, 2 };
+int[] nums2 = { 1, 2, 3 };
+
+
+Print(PermuteUnique(nums1));
+Print(PermuteUnique(nums2));
+
+Console.ReadLine();
+
+
+IList<IList<int>> PermuteUnique(int[] nums)
+{
+    IList<IList<int>> result = new List<IList<int>>();
+
+    return result;
+}
+
+
+void Print(IList<IList<int>> lists)
+{
+    Console.WriteLine();
+    Console.WriteLine();
+    foreach (var list in lists)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(list[i]);
+        }
+        Console.WriteLine();
+    }
+}
