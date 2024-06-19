799426f6bd898c9fa413de9797ab033494985cf5
Alexander <sany199574@gmail.com>
2024-06-19 19:42:44 +0300

    Добавлен алгоритм

--- project_48_Rotate Image/Program.cs
+++ project_48_Rotate Image/Program.cs
@@ -1,5 +1,4 @@
 ﻿
-
 int[][] nums1 = new int[][]
 {
     new int[3]{ 1,2,3},
@@ -7,7 +6,13 @@ int[][] nums1 = new int[][]
     new int[3]{ 7,8,9}
 };
 
+int[][] nums2 = new int[][]
+{
+    new int[1]{ 1}
+};
+
 Rotate(nums1);
+Rotate(nums2);
 
 
 
@@ -16,6 +21,37 @@ Console.ReadLine();
 
 void Rotate(int[][] matrix)
 {
+    //if(matrix == null || matrix.Length < 1 || matrix.GetLength(1) <= 1) { return; }
 
+    int[][] array = GetCoppy(matrix);
+
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = matrix.Length - 1, k = 0; j >= 0; j--,k++)
+        {
+            matrix[i][k] = array[j][i];
+            Console.Write(matrix[i][k] + " ");
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 }
 
+int[][] GetCoppy(int[][] matrix)
+{
+    int[][] array = new int[matrix.Length][];
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        array[i] = new int[matrix.Length];
+    }
+
+    for (int i = 0;i < matrix.Length; i++)
+    {
+        for(int j = 0;j < matrix.Length; j++)
+        {
+            array[i][j] = matrix[i][j];
+        }
+    }
+    return array;
+}
