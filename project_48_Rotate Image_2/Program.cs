dc6d47fe6b777996b4ebb64424fb708d2a39c7fd
Alexander <sany199574@gmail.com>
2024-06-19 22:36:06 +0300

    Добавлено приложение для разворота картинки

---
+++ project_48_Rotate Image_2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[][] nums1 = new int[][]
+{
+    new int[3]{ 1,2,3},
+    new int[3]{ 4,5,6},
+    new int[3]{ 7,8,9}
+};
+
+int[][] nums2 = new int[][]
+{
+    new int[1]{ 1}
+};
+
+Rotate(nums1);
+Rotate(nums2);
+
+
+
+Console.ReadLine();
+
+
+void Rotate(int[][] matrix)
+{
+
+}
