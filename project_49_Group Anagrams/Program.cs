6447cae82dad5dd01699a462fc6ea72b3e5c7dda
Alexander <sany199574@gmail.com>
2024-06-21 15:58:47 +0300

    Добавлен алгоритм

---
+++ project_49_Group Anagrams/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Text;
+
+string[] str1 = { "eat", "tea", "tan", "ate", "nat", "bat" };
+string[] str2 = { "eat", "tea", "tan", "ate", "nat", "bat" };
+string[] str3 = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+GroupAnagrams(str1);
+GroupAnagrams(str2);
+GroupAnagrams(str3);
+
+
+Console.ReadLine();
+
+
+IList<IList<string>> GroupAnagrams(string[] strs)
+{
+    List<IList<string>> result = new List<IList<string>>();
+
+    Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+    foreach (string str in strs)
+    {
+        string word = string.Concat(str.Order().ToArray());
+
+        if(dict.ContainsKey(word))
+        {
+            dict[word].Add(str);
+        }
+        else
+        {
+            List<string> list = new List<string>();
+            list.Add(str);
+            dict.Add(word, list);
+        }
+
+        
+    }
+
+    result.AddRange(dict.Values);
+
+    return result;
+}
---
+++ project_49_Group Anagrams_2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Text;
+
+string[] str1 = { "eat", "tea", "tan", "ate", "nat", "bat" };
+string[] str2 = { "eat", "tea", "tan", "ate", "nat", "bat" };
+string[] str3 = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+GroupAnagrams(str1);
+GroupAnagrams(str2);
+GroupAnagrams(str3);
+
+
+Console.ReadLine();
+
+
+IList<IList<string>> GroupAnagrams(string[] strs)
+{
+    Dictionary<string, IList<string>> result = new();
+    foreach (var s in strs)
+    {
+        var chars = s.ToCharArray();
+        Array.Sort(chars);
+        var key = new string(chars);
+        if (!result.TryAdd(key, new List<string>() { s }))
+            result[key].Add(s);
+    }
+    return result.Values.ToList();
+}
