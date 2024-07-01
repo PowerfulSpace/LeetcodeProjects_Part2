3c9f8246fbe7457d4e4dba67731dcd24f6d5d3bf
Alexander <sany199574@gmail.com>
2024-07-01 19:42:27 +0300

    Добавлена алоьтернативная вариация кода

--- project_51_N-Queens/Program.cs
+++ project_51_N-Queens/Program.cs
@@ -1,9 +1,13 @@
 ﻿
-int n1 = 9;
+int n1 = 4;
 
 SolveNQueens(n1);
 
 Console.ReadLine();
+
+
+// Решить вопрос, с бесконечным циклом
+
 
 IList<IList<string>> SolveNQueens(int n)
 {
@@ -11,56 +15,106 @@ IList<IList<string>> SolveNQueens(int n)
 
     char[,] CheasBoard = new char[n,n];
 
+    int countQueens = 0;
+
     int x = 0;
     int y = 0;
 
-    for (int i = 0; i < CheasBoard.GetLength(0); i++)
-    {
-        for (int j = 0; j < CheasBoard.GetLength(1); j++)
-        {
-            if(CheasBoard[i, j] == '.') { continue; };
+    int left = 0;
+    int rigth = 0;
 
-            if (CheasBoard[i, j] != '.' && CheasBoard[i, j] != 'Q')
+  
+    AccommodationQueens(CheasBoard,y,x,left,rigth, countQueens);
+
+    return result;
+}
+
+bool AccommodationQueens(char[,] CheasBoard, int y, int x, int left, int rigth, int countQueens)
+{
+    if(y > CheasBoard.GetLength(0) || x > CheasBoard.GetLength(0))
+    {
+        return true;
+    }
+
+    if (countQueens == CheasBoard.GetLength(0))
+    {
+        return true;
+    }
+    bool isAdd = false;
+
+    if (y < CheasBoard.GetLength(1))
+    {
+        for (int j = 0; j < CheasBoard.GetLength(0); j++)
+        {
+            if (CheasBoard[y, j] == '.') { continue; };
+
+            if (CheasBoard[y, j] != '.' && CheasBoard[y, j] != 'Q')
             {
-                CheasBoard[i, j] = 'Q';
-               
-                y = i;
+                CheasBoard[y, j] = 'Q';
+                isAdd = true;
                 x = j;
 
                 break;
             }
         }
 
-        int left = x - y;
-        int rigth = x + y;
+        if (isAdd == false) { return false; }
+
+        left = x - y;
+        rigth = x + y;
+
+        LokingCells(CheasBoard, y, x, left, rigth);
+
+        Print(CheasBoard);
 
 
-        for (int j = 0; j < CheasBoard.GetLength(0); j++)
+        bool result = AccommodationQueens(CheasBoard, y + 1, x, left, rigth, countQueens);
+
+        if (result == false)
         {
-            if (CheasBoard[y, j] != 'Q') { CheasBoard[y, j] = '.'; }
-            if (CheasBoard[j, x] != 'Q') { CheasBoard[j, x] = '.'; }
-
-            if(left >= 0 && left <= CheasBoard.GetLength(1) - 1)
-            {
-                if (CheasBoard[y, left] != 'Q') { CheasBoard[j, left] = '.'; } 
-            }
-            left++;
-
-            if (rigth <= CheasBoard.GetLength(1) - 1 && rigth >= 0)
-            {
-                if (CheasBoard[y, rigth] != 'Q') { CheasBoard[j, rigth] = '.'; }
-            }
-            rigth--;
+            CheasBoard[y, x] = '\0';
+            AccommodationQueens(CheasBoard, y, x + 1, left, rigth, countQueens);
         }
     }
 
-    AccommodationQueens(CheasBoard,0,0);
-
-    return result;
+    return true;
 }
 
-void AccommodationQueens(char[,] CheasBoard, int y, int x)
+void LokingCells(char[,] CheasBoard, int y, int x, int left, int rigth)
 {
-    //Добавить рекурсивный метод, для откатывания назад вигур.
-    //Откатанная фигура, должна будет сместиться на следующий свободный лемент в право
+    
+    for (int j = 0; j < CheasBoard.GetLength(0); j++)
+    {
+        if (CheasBoard[y, j] != 'Q') { CheasBoard[y, j] = '.'; }
+        if (CheasBoard[j, x] != 'Q') { CheasBoard[j, x] = '.'; }
+
+        if (left >= 0 && left <= CheasBoard.GetLength(1) - 1)
+        {
+            if (CheasBoard[y, left] != 'Q' && CheasBoard[j, left] != 'Q') { CheasBoard[j, left] = '.'; }
+        }
+        left++;
+
+        if (rigth <= CheasBoard.GetLength(1) - 1 && rigth >= 0)
+        {
+            if (CheasBoard[y, rigth] != 'Q' && CheasBoard[j, rigth] != 'Q') { CheasBoard[j, rigth] = '.'; }
+        }
+        rigth--;
+    }
+}
+
+
+//Добавить рекурсивный метод, для откатывания назад вигур.
+//Откатанная фигура, должна будет сместиться на следующий свободный лемент в право
+
+
+void Print(char[,] CheasBoard)
+{
+    for (int i = 0; i < CheasBoard.GetLength(0); i++)
+    {
+        for (int j = 0; j < CheasBoard.GetLength(1); j++)
+        {
+            Console.Write(CheasBoard[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
