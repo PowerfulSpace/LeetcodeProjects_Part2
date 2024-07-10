254d4b52dc0dad13d1752a828792fefdff9bd44d
Alexander <sany199574@gmail.com>
2024-07-10 04:08:11 +0300

    Добавлен алгоритм

--- project_56_ Merge Intervals/Program.cs
+++ project_56_ Merge Intervals/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Collections.Generic;
+
 int[][] nums1 = new int[4][]
 {
     new int[]{1,3 },
@@ -20,10 +22,9 @@ int[][] nums3 = new int[2][]
     new int[]{5,6 },
 };
 
-//Console.WriteLine(Merge(nums1));
-//Console.WriteLine(Merge(nums2));
-Console.WriteLine(Merge(nums3));
-
+Merge(nums1);
+Merge(nums2);
+Merge(nums3);
 
 Console.ReadLine();
 
@@ -31,31 +32,28 @@ int[][] Merge(int[][] intervals)
 {
     if(intervals.Length == 1) { return intervals; }
 
-    List<List<int>> result = new List<List<int>>();
+    intervals = intervals.OrderBy(x => x[0]).ToArray();
 
-    int[] array = intervals[0];
+    List<int[]> result = new List<int[]>();
+
+    result.Add(intervals[0]);
 
     foreach (int[] arrayWithIntervals in intervals.Skip(1))
     {
+        int[] array = result.Last();
         if (array[1] >= arrayWithIntervals[0])
         {
-            result.Add(new List<int>() { array[0], arrayWithIntervals[1] });
+            array[0] = array[0] < arrayWithIntervals[0] ? array[0] : arrayWithIntervals[0];
+            array[1] = array[1] > arrayWithIntervals[1] ? array[1] : arrayWithIntervals[1];
+
             intervals.Skip(1);
         }
         else
         {
-            result.Add(new List<int>(arrayWithIntervals));
+            result.Add(arrayWithIntervals);
         }
+
     }
 
-    intervals = new int[result.Count][];
-    int index = 0;
-    foreach (var item in result)
-    {
-        intervals[index] = item.ToArray();
-        index++;
-    }
-
-
-    return intervals;
+    return result.ToArray();
 }
