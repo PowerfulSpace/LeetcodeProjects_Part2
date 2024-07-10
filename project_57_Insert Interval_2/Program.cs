d6f8fb02c568c145d0b1b809a24e2e6c0fe7753f
Alexander <sany199574@gmail.com>
2024-07-10 05:00:01 +0300

    Добавлен тестовый пример

---
+++ project_57_Insert Interval_2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+int[][] nums1 = new int[2][]
+{
+    new int[]{1,3 },
+    new int[]{6,9 }
+
+};
+
+int[][] nums2 = new int[5][]
+{
+    new int[]{1,2 },
+    new int[]{3,5},
+    new int[]{6,7},
+    new int[]{8,10},
+    new int[]{12,16}
+};
+int[][] nums3 = new int[1][]
+{
+    new int[]{1,5 },
+
+};
+
+Insert(nums1, new int[] { 2, 5 });
+Insert(nums2, new int[] { 4, 8 });
+Insert(nums3, new int[] { 2, 7 });
+
+
+Console.ReadLine();
+
+int[][] Insert(int[][] intervals, int[] newInterval)
+{
+ 
+
+    List<int[]> result = new List<int[]>();
+
+  
+
+    return result.ToArray();
+}
