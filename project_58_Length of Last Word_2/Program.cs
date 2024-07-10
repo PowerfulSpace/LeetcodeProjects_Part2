728e9dc7ef1f39fa40c25a095726dbcb618235c4
Alexander <sany199574@gmail.com>
2024-07-10 23:31:57 +0300

    Добавлена задача на спираль

---
+++ project_58_Length of Last Word_2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string input1 = "Hello World";
+string input2 = "   fly me   to   the moon  ";
+string input3 = "a ";
+
+Console.WriteLine(LengthOfLastWord(input1));
+Console.WriteLine(LengthOfLastWord(input2));
+Console.WriteLine(LengthOfLastWord(input3));
+
+
+
+
+Console.ReadLine();
+
+
+int LengthOfLastWord(string s)
+{
+    s = s.Trim();
+    int count = 0;
+
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        if (s[i] == ' ') { break; }       
+        count++;
+    }
+
+    return count;
+}
---
+++ project_59_Spiral Matrix II/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int num1 = 3;
+int num2 = 1;
+
+GenerateMatrix(num1);
+GenerateMatrix(num2);
+
+Console.ReadLine();
+
+
+int[][] GenerateMatrix(int n)
+{
+    return new int[n][];
+}
+
+
