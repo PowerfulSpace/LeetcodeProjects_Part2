26ab0b80abc884026736b1d3ddd46680ef2d8e91
Alexander <sany199574@gmail.com>
2024-07-11 04:17:39 +0300

    Добавлен алгоритм

--- project_59_Spiral Matrix II/Program.cs
+++ project_59_Spiral Matrix II/Program.cs
@@ -11,10 +11,58 @@ Console.ReadLine();
 
 int[][] GenerateMatrix(int n)
 {
-    List<int> result = new List<int>();
+    int[][] result = CreateArray(n);
+    int num = 1;
+
+    int left = 0;
+    int top = 0;
+    int right = n - 1;
+    int bot = n - 1;
+
+    while (left <= right && top <= bot)
+    {
+        for (int i = left; i <= right; i++)
+        {
+            result[top][i] = num;
+            num++;
+        }
+        top++;
+
+        for (int i = top; i <= bot; i++)
+        {
+            result[i][right] = num;
+            num++;
+        }
+        right--;
+
+        for (int i = right; i >= left; i--)
+        {
+            result[bot][i] = num;
+            num++;
+        }
+        bot--;
+
+        for (int i = bot; i >= top; i--)
+        {
+            result[i][left] = num;
+            num++;
+        }
+        left++;
+    }
 
 
-    return new int[n][];
+
+    return result;
 }
 
 
+int[][] CreateArray(int n)
+{
+
+    int[][] result = new int[n][];
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = new int[n];
+    }
+    return result;
+}
