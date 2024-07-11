39a4bfb4312c732b13eb542452d94065663adb7b
Alexander <sany199574@gmail.com>
2024-07-11 08:25:33 +0300

    Добавлен алгоритм

--- project_60_Permutation Sequence/Program.cs
+++ project_60_Permutation Sequence/Program.cs
@@ -6,39 +6,44 @@ int n1 = 3, k1 = 3;
 int n2 = 4, k2 = 9;
 int n3 = 3, k3 = 1;
 
-Console.WriteLine(GetPermutation(n1,k1));
-//Console.WriteLine(GetPermutation(n2,k2));
-//Console.WriteLine(GetPermutation(n3,k3));
+Console.WriteLine(GetPermutation(n1, k1));
+Console.WriteLine(GetPermutation(n2, k2));
+Console.WriteLine(GetPermutation(n3, k3));
 
 Console.ReadLine();
 
 
 string GetPermutation(int n, int k)
 {
-    string result = GetSequence(new StringBuilder(),1,n,k,0, "");
-    return result;
+    StringBuilder sb = new StringBuilder();
+    GetSequence(new HashSet<int>(), n, k, new int[1], sb);
+    return sb.ToString();
 }
 
 
-string GetSequence(StringBuilder s,int index, int n, int k, int count,string result)
+void GetSequence(HashSet<int> s, int n, int k, int[] count, StringBuilder sb)
 {
-    if(s.Length == 3)
+    if (s.Count == n)
     {
-        count++;
-        if (count == k)
+        count[0]++;
+        if (count[0] == k)
         {
-            result = s.ToString();
+            sb.Append(string.Concat(s));
         }
-        return result;
+        return;
     }
 
-    for (int i = index; i <= n;i++)
+    for (int i = 1; i <= n; i++)
     {
-        s.Append(i);
-        GetSequence(s,i + 1,n, k,count,result);
-        s.Length--;
+        if (s.Add(i))
+        {
+            GetSequence(s, n, k, count, sb);
+
+            s.Remove(i);
+
+            if(sb.Length > 0) { return; }
+        }
     }
 
-    return result;
+    return;
 }
-
