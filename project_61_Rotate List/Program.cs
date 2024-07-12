9a098f6475486a7f2f8d0f1fb58286327c975f0b
Alexander <sany199574@gmail.com>
2024-07-12 03:28:20 +0300

    Добавлен алгоритм

--- project_61_Rotate List/Program.cs
+++ project_61_Rotate List/Program.cs
@@ -3,26 +3,48 @@
 ListNode head1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
 int k1 = 2;
 
+ListNode head2 = new ListNode(0, new ListNode(1, new ListNode(2)));
+int k2 = 4;
+
 RotateRight(head1, k1);
+RotateRight(head2, k2);
 
 
 Console.ReadLine();
 
 ListNode RotateRight(ListNode head, int k)
 {
-    ListNode left = head;
-    ListNode newHead = new ListNode();
+    ListNode first = head;
+    ListNode last = new ListNode();
+    int index = 0;
 
-    while(k != 0)
+    while(first != null)
     {
-        k--;
-        if (k == 0)
+        index++;
+        if (first.next == null)
         {
-            newHead = head;
-        }
-        head = head.next;
+            last = first;
+            first.next = head;
+            break;
+        }       
+        first = first.next;
     }
 
+    if(index >= k)
+    {
+        int skip = Math.Abs(index - k);
+
+        while (skip > 0)
+        {
+            head = head.next;
+            skip--;
+        }
+    }
+    else
+    {
+        return last;
+    }
+   
 
     return head;    
 }
