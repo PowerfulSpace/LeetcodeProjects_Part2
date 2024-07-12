8016a4dd698c4d950d46860fdf65e342e09262eb
Alexander <sany199574@gmail.com>
2024-07-13 02:38:59 +0300

    Добавлена задача

---
+++ project_62_Unique Paths_2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+int m1 = 3, n1 = 7;
+int m2 = 3, n2 = 2;
+
+Console.WriteLine(UniquePaths(m1, n1));
+Console.WriteLine(UniquePaths(m2, n2));
+
+
+Console.ReadLine();
+
+
+
+
+int UniquePaths(int m, int n)
+{
+    int result = GetFoundPaths(m,n, 0, 0, 0);
+
+    return result;
+}
+
+int GetFoundPaths(int yLength,int xLength, int cY, int cX, int count)
+{
+    if (cY > yLength - 1) { return count; }
+    if (cY == yLength - 1 && cX == xLength - 1)
+    {
+        count++;
+        return count;
+    }
+
+    for (int x = cX; x < xLength; x++)
+    {
+        if (cY < yLength)
+        {
+            count = GetFoundPaths(yLength, xLength, cY, x + 1, count);
+        }
+        count = GetFoundPaths(yLength, xLength, cY + 1, cX, count);
+
+        return count;
+    }
+
+    return count;
+}
