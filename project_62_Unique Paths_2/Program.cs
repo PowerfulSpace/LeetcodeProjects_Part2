0566dc23cc9858a439a2b29f0f35e43d4ba75fd8
Alexander <sany199574@gmail.com>
2024-07-13 03:39:00 +0300

    Добавлена задача

--- project_62_Unique Paths_2/Program.cs
+++ project_62_Unique Paths_2/Program.cs
@@ -3,9 +3,25 @@
 
 int m1 = 3, n1 = 7;
 int m2 = 3, n2 = 2;
+int m3 = 23, n3 = 12;
 
-Console.WriteLine(UniquePaths(m1, n1));
-Console.WriteLine(UniquePaths(m2, n2));
+int m4 = 3, n4 = 7;
+int m5 = 4, n5 = 8;
+int m6 = 5, n6 = 9;
+
+Console.WriteLine(UniquePaths(m4, n4));
+Console.WriteLine(UniquePaths(m5, n5));
+Console.WriteLine(UniquePaths(m6, n6));
+
+//Console.WriteLine(UniquePaths(m1, n1));
+//Console.WriteLine(UniquePaths(m2, n2));
+
+//var sw = new Stopwatch();
+
+//sw.Start();
+//Console.WriteLine(UniquePaths(m3, n3));
+//sw.Stop();
+//Console.WriteLine(sw.Elapsed);
 
 
 Console.ReadLine();
---
+++ project_62_Unique Paths_3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+int m1 = 3, n1 = 7;
+int m2 = 3, n2 = 2;
+int m3 = 23, n3 = 12;
+
+
+Console.WriteLine(UniquePaths(m1, n1));
+Console.WriteLine(UniquePaths(m2, n2));
+Console.WriteLine(UniquePaths(m3, n3));
+
+
+Console.ReadLine();
+
+
+
+
+int UniquePaths(int m, int n)
+{
+
+    return 1;
+}
