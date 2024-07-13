ca149d778c8199f6187dd327f684aae8d6883cd1
Alexander <sany199574@gmail.com>
2024-07-13 04:26:43 +0300

    Добавлена задача

---
+++ project_63_Unique Paths II/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[][] array1 = new int[3][]
+{
+    new int[3] {0,0,0},
+    new int[3] {0,1,0},
+    new int[3] {0,0,0}
+};
+
+Console.WriteLine(UniquePathsWithObstacles(array1));
+
+Console.ReadLine();
+int UniquePathsWithObstacles(int[][] obstacleGrid)
+{
+    return 1;
+}
