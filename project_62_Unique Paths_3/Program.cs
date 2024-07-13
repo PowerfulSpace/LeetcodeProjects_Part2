72cfc0250ca3a6eb01efb041803e5f8833684889
Alexander <sany199574@gmail.com>
2024-07-13 04:23:29 +0300

    Добавлен алгоритм

--- project_62_Unique Paths_3/Program.cs
+++ project_62_Unique Paths_3/Program.cs
@@ -18,6 +18,31 @@ Console.ReadLine();
 
 int UniquePaths(int m, int n)
 {
+    int[,] array = Filling(m,n);
 
-    return 1;
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            array[i, j] = array[i - 1, j] + array[i, j - 1];
+        }
+    }
+
+    return array[m - 1, n - 1];
 }
+
+
+int[,] Filling(int m,int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < n; i++)
+    {
+        array[0, i] = 1;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        array[i, 0] = 1;
+    }
+    return array;
+}
+
---
+++ project_62_Unique Paths_4/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+int m1 = 3, n1 = 7;
+int m2 = 3, n2 = 2;
+int m3 = 23, n3 = 12;
+
+
+Console.WriteLine(UniquePaths(m1, n1));
+Console.WriteLine(UniquePaths(m2, n2));
+Console.WriteLine(UniquePaths(m3, n3));
+
+
+Console.ReadLine();
+
+
+
+
+int UniquePaths(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                array[i, j] = 1;
+            }
+            else
+            {
+                array[i, j] = array[i - 1, j] + array[i, j - 1];
+            }
+        }
+    }
+
+    return array[m - 1, n - 1];
+}
