e358304d6a93a846b0163496d37f94c41501712b
Alexander <sany199574@gmail.com>
2024-07-16 03:53:53 +0300

    Изменён алгоритм

--- project_64_Minimum Path Sum/Program.cs
+++ project_64_Minimum Path Sum/Program.cs
@@ -6,8 +6,21 @@ int[][] grid1 = new int[3][]
     new int[3] {1,5,1 },
     new int[3] {4,2,1 }
 };
+int[][] grid2 = new int[2][]
+{
+    new int[3] {1,2,3 },
+    new int[3] {4,5,6}
+};
+
+int[][] grid3 = new int[2][]
+{
+    new int[2] {1,2 },
+    new int[2] {1,1 }
+};
 
 Console.WriteLine(MinPathSum(grid1));
+Console.WriteLine(MinPathSum(grid2));
+Console.WriteLine(MinPathSum(grid3));
 
 
 Console.ReadLine();
@@ -15,37 +28,37 @@ Console.ReadLine();
 
 int MinPathSum(int[][] grid)
 {
-
-    int result = GetPathSum(grid, 0,0,0);
-    return result;
+    int[] array = { int.MaxValue };
+    int result = GetPathSum(grid, 0, 0, 0, array);
+    return array[0];
 }
 
-int GetPathSum(int[][] grid, int y, int x, int sum)
+int GetPathSum(int[][] grid, int y, int x, int sum, int[] minSum)
 {
-    if (y > grid.Length) { return sum; }
-    if (y == grid.Length && x == grid[0].Length - 1)
-    { 
+    if (y > grid.Length || x > grid.Length) { return sum; }
+
+    if (y == grid.Length - 1 && x == grid[0].Length - 1)
+    {
+        sum += grid[y][x];
+        minSum[0] = Math.Min(sum, minSum[0]);
+
         return sum;
     }
 
-    for (int i = x; i < grid.Length; i++)
+    for (int i = x; i < grid[0].Length; i++)
     {
-        int result = 0;
-
-        if(y < grid.Length)
+        if (y < grid.Length)
         {
             sum += grid[y][i];
-            sum = GetPathSum(grid, y, i + 1, sum);
+            sum = GetPathSum(grid, y, i + 1, sum, minSum);
         }
-        
-        result = GetPathSum(grid, y + 1, i, sum);
 
-        sum = Math.Min(sum, result);
+        sum = GetPathSum(grid, y + 1, i, sum, minSum);
 
         if (y < grid.Length)
         {
             sum -= grid[y][i];
-        }  
+        }
     }
 
     return sum;
