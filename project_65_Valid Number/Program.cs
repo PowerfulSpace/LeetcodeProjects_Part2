baafa0a76c2fdb540d53ad2f7ab8186fab27eaa5
Alexander <sany199574@gmail.com>
2024-07-18 11:17:08 +0300

    Добавлен алгоритм

--- project_65_Valid Number/Program.cs
+++ project_65_Valid Number/Program.cs
@@ -1,19 +1,27 @@
 ﻿
+using System.Text.RegularExpressions;
+
 string s1 = "0";
 string s2 = "e";
 string s3 = ".";
+string s4 = "3.";
+string s5 = "e9.";
+string s6 = "0e";
 
 
-Console.WriteLine(IsNumber(s1));
-Console.WriteLine(IsNumber(s2));
-Console.WriteLine(IsNumber(s3));
+//Console.WriteLine(IsNumber(s1));
+//Console.WriteLine(IsNumber(s2));
+//Console.WriteLine(IsNumber(s3));
+//Console.WriteLine(IsNumber(s4));
+//Console.WriteLine(IsNumber(s5));
+Console.WriteLine(IsNumber(s6));
 
 
 Console.ReadLine();
 
 
-
 bool IsNumber(string s)
 {
-    return Char.IsDigit(s.ToCharArray()[0]);
+    Regex reg = new Regex(@"^\s*[+-]?(\d+(\.\d*)?|\.\d+)([Ee][+-]?\d+)?\s*$");
+    return reg.IsMatch(s);
 }
