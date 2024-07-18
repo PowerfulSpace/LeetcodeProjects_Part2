09b6867de99fb35b1ab025562704ee1920f1c566
Alexander <sany199574@gmail.com>
2024-07-18 10:15:47 +0300

    Добавлен алгоритм

---
+++ project_65_Valid Number/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string s1 = "0";
+string s2 = "e";
+string s3 = ".";
+
+
+Console.WriteLine(IsNumber(s1));
+Console.WriteLine(IsNumber(s2));
+Console.WriteLine(IsNumber(s3));
+
+
+Console.ReadLine();
+
+
+
+bool IsNumber(string s)
+{
+    return Char.IsDigit(s.ToCharArray()[0]);
+}
