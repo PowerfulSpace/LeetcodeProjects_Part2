e975778a384b8d29b2aebc28566c476b262b7364
Alexander <sany199574@gmail.com>
2024-07-19 09:49:01 +0300

    Lj,fdkty fkujhbn

--- project_67_Add Binary/Program.cs
+++ project_67_Add Binary/Program.cs
@@ -1,8 +1,12 @@
 ﻿
-string s1 = "";
-string s2 = "";
+string a1 = "11";
+string b1 = "1";
 
-Console.WriteLine(AddBinary(s1,s2));
+string a2 = "1010";
+string b2 = "1011";
+
+Console.WriteLine(AddBinary(a1,b1));
+Console.WriteLine(AddBinary(a2,b2));
 
 Console.ReadLine();
 
@@ -12,5 +16,43 @@ Console.ReadLine();
 
 string AddBinary(string a, string b)
 {
-    return a;
+	string result = string.Empty;
+	Dictionary<int, int> binary = new Dictionary<int, int>()
+	{
+		[0] = 1,
+		[1] = 2,
+        [2] = 4,
+        [3] = 8,
+        [4] = 16,
+        [5] = 32,
+        [6] = 64,
+        [7] = 128
+    };
+    List<string> list = new List<string>();
+	
+	list.Add(a);
+	list.Add(b);
+
+	int[] nums = new int[2];
+	int indexNum = 0;
+	int indexBinary = 0;
+
+	foreach (string s in list)
+	{
+		int num = 0;
+		indexBinary = 0;
+        for (int i = s.Length - 1; i >= 0; i--, indexBinary++)
+        {
+			if (s[i] == '1')
+			{
+				num += binary[indexBinary];
+            }
+        }
+		nums[indexNum] = num;
+		indexNum++;
+    }
+	
+
+
+    return result;
 }
