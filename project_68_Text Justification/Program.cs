26c2d3e0c82a4d0d6f7c872cfae912d1b12ca2c0
Alexander <sany199574@gmail.com>
2024-07-20 14:27:31 +0300

    Добавлена задача

---
+++ project_68_Text Justification/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string[] str1 = { "This", "is", "an", "example", "of", "text", "justification." };
+int maxWidth1 = 16;
+
+
+
+Console.ReadLine();
+
+
+IList<string> FullJustify(string[] words, int maxWidth)
+{
+    List<string> result = new List<string>();
+
+
+
+    return result;
+}
+
+
