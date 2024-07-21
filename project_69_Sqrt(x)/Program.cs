4607438b473981d978d1e388313bde8e88d088f3
Alexander <sany199574@gmail.com>
2024-07-21 12:07:25 +0300

    Добавлен алгоритм

---
+++ project_69_Sqrt(x)/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int x1 = 4;
+int x2 = 8;
+
+
+Console.WriteLine(MySqrt(x1));
+Console.WriteLine(MySqrt(x2));
+
+
+Console.ReadLine();
+
+
+int MySqrt(int x)
+{
+    return (int)Math.Sqrt(x);
+}
+
+
