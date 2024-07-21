2ee17deec66f9258caf50b9463c357e5654dbc81
Alexander <sany199574@gmail.com>
2024-07-21 17:35:09 +0300

    Добавлен алгоритм

--- project_70_Climbing Stairs/Program.cs
+++ project_70_Climbing Stairs/Program.cs
@@ -3,7 +3,7 @@ int n1 = 2;
 int n2 = 3;
 int n3 = 5;
 
-Console.WriteLine(ClimbStairs(n1));
+//Console.WriteLine(ClimbStairs(n1));
 Console.WriteLine(ClimbStairs(n2));
 Console.WriteLine(ClimbStairs(n3));
 
@@ -11,31 +11,27 @@ Console.ReadLine();
 
 int ClimbStairs(int n)
 {
-    int data = 0;
-    int result = CalculateSteps(n,1,0, ref data);
+    int result = 0;
+    int count = CalculateSteps(n,0, ref result);
 
-    return data;
+    return result;
 }
 
 
 
-int CalculateSteps(int stairs,int step, int count,ref int result)
+int CalculateSteps(int stairs,int count,ref int result)
 {
-
-    if(count > stairs)
+    if(count > stairs) { return count; }
+    if(count == stairs)
     {
-        return count;
-    }
-    if (count == stairs)
-    {
-        result += 1;
+        result++;
         return count;
     }
 
-    for (int i = step; i < 3; i++)
+    for (int i = 1; i < 3; i++)
     {
         count += i;
-        count = CalculateSteps(stairs, i, count,ref result);
+        count = CalculateSteps(stairs, count, ref result);
         count -= i;       
     }
 
