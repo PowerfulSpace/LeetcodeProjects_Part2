7ad34d6d15628d07a33085bbbff87755d859af9a
Alexander <sany199574@gmail.com>
2024-07-22 11:40:57 +0300

    Удалён лишний код

--- project_70_Climbing Stairs/Program.cs
+++ project_70_Climbing Stairs/Program.cs
@@ -3,7 +3,7 @@ int n1 = 2;
 int n2 = 3;
 int n3 = 5;
 
-//Console.WriteLine(ClimbStairs(n1));
+Console.WriteLine(ClimbStairs(n1));
 Console.WriteLine(ClimbStairs(n2));
 Console.WriteLine(ClimbStairs(n3));
 
---
+++ project_70_Climbing Stairs_2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int n1 = 2;
+int n2 = 3;
+int n3 = 5;
+
+Console.WriteLine(ClimbStairs(n1));
+Console.WriteLine(ClimbStairs(n2));
+Console.WriteLine(ClimbStairs(n3));
+
+Console.ReadLine();
+
+int ClimbStairs(int n)
+{
+
+
+    return n;
+}
+
+
