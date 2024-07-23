86cb8f982edbefd56a010eed7eb39082c2200dfd
Alexander <sany199574@gmail.com>
2024-07-23 14:32:27 +0300

    Добавлен алгоритм

--- project_71_Simplify Path_2/Program.cs
+++ project_71_Simplify Path_2/Program.cs
@@ -9,12 +9,12 @@ string path7 = "/.../a/../b/c/../d/./";
 string path8 = "/../..ga/b/.f..d/..../e.baaeeh./.a";
 
 //Console.WriteLine(SimplifyPath(path1));
-//Console.WriteLine(SimplifyPath(path2));
-//Console.WriteLine(SimplifyPath(path3));
-//Console.WriteLine(SimplifyPath(path4));
-//Console.WriteLine(SimplifyPath(path5));
-//Console.WriteLine(SimplifyPath(path6));
-//Console.WriteLine(SimplifyPath(path7));
+Console.WriteLine(SimplifyPath(path2));
+Console.WriteLine(SimplifyPath(path3));
+Console.WriteLine(SimplifyPath(path4));
+Console.WriteLine(SimplifyPath(path5));
+Console.WriteLine(SimplifyPath(path6));
+Console.WriteLine(SimplifyPath(path7));
 Console.WriteLine(SimplifyPath(path8));
 
 
@@ -23,10 +23,33 @@ Console.ReadLine();
 
 string SimplifyPath(string path)
 {
+    Stack<string> stack = new Stack<string>();
+    var split = path.Split('/');
+    for (int i = 1; i < split.Count(); i++)
+    {
+        if (split[i].Length == 0)
+            continue;
+        if (split[i].Length == 1 && split[i][0] == '.')
+        {
+            Console.WriteLine($"Split1 = {split[i]}");
+            continue;
+        }
 
-    string result = " ";
+        else if (split[i].Length == 2 && split[i][0] == '.' && split[i][1] == '.')
+        {
 
-    if (result == "") { result = "/"; }
+            Console.WriteLine($"Split2 = {split[i]}");
+            if (stack.Count() > 0)
+                stack.Pop();
+        }
 
-    return result;
+        else
+        {
+            Console.WriteLine($"Pushing = {split[i]}");
+            stack.Push(split[i]);
+        }
+
+    }
+
+    return "/" + String.Join("/", stack.Reverse());
 }
