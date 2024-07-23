dd57feebd0d21406975e32d31dab30e3fc4fdcf3
Alexander <sany199574@gmail.com>
2024-07-23 23:43:02 +0300

    Добавлен вспомогательный комментарий

--- project_72_Edit Distance/Program.cs
+++ project_72_Edit Distance/Program.cs
@@ -10,28 +10,13 @@ Console.WriteLine(MinDistance(word1, word2));
 
 Console.ReadLine();
 
-
+//Использщовавать подхзод динамического программирования
 int MinDistance(string word1, string word2)
 {
     StringBuilder sb = new StringBuilder(word1);
     int count = 0;
 
-    for (int i = 0; i < word1.Length; i++)
-    {
-        if (i >= word2.Length)
-        {
-            while(sb.Length > word2.Length)
-            {
-                sb.Length--;
-            }
-            break;  
-        }
-        if (sb[i] != word2[i])
-        {
-            sb[i] = word2[i];
-            count++;
-        }
-    }
+    
 
 
     return count;
