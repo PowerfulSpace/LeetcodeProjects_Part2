414f85a3ea6a05f818f6eac14b7c4280ed1012dd
Alexander <sany199574@gmail.com>
2024-07-24 20:29:10 +0300

    Добавлена задача

---
+++ project_73_Set Matrix Zeroes/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int[][] array1 = new int[3][]
+{
+    new int[3]{1,1,1},
+    new int[3]{1,0,1},
+    new int[3]{1,1,1}
+};
+
+int[][] array2 = new int[3][]
+{
+    new int[4]{0,1,2,0},
+    new int[4]{3,4,5,2},
+    new int[4]{1,3,1,5}
+};
+
+SetZeroes(array1);
+SetZeroes(array2);
+
+
+Console.ReadLine();
+
+
+
+void SetZeroes(int[][] matrix)
+{
+
+}
