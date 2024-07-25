342f08a816426f4553458a1c91318ab737b6a679
Alexander <sany199574@gmail.com>
2024-07-26 01:00:58 +0300

    Добавлен алгоритм

---
+++ project_75_Sort Colors_3/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+int[] nums1 = { 2, 0, 2, 1, 1, 0 };
+int[] nums2 = { 2, 0, 1 };
+
+SortColors(nums1);
+
+Console.ReadLine();
+
+
+void SortColors(int[] nums)
+{
+    int low = 0, mid = 0, high = nums.Length - 1;
+
+    while (mid <= high)
+    {
+        if (nums[mid] == 0)
+        {
+            Swap(nums, low, mid);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            Swap(nums, mid, high);
+            high--;
+        }
+    }
+}
+void Swap(int[] nums, int i, int j)
+{
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
---
+++ project_76_Minimum Window Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string s1 = "ADOBECODEBANC", t1 = "ABC";
+string s2 = "a", t2 = "a";
+string s3 = "a", t3 = "aa";
+
+
+Console.WriteLine(MinWindow(s1,t1));
+Console.WriteLine(MinWindow(s2,t2));
+Console.WriteLine(MinWindow(s3,t3));
+
+
+Console.ReadLine();
+
+
+string MinWindow(string s, string t)
+{
+    return "1";
+}
+
+
