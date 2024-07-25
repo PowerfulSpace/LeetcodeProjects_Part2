c8f4a0c8bea12620d67e9c64e3e06ce77dcccb5d
Alexander <sany199574@gmail.com>
2024-07-25 18:16:46 +0300

    Добавлен алгоритм

--- project_73_Set Matrix Zeroes/Program.cs
+++ project_73_Set Matrix Zeroes/Program.cs
@@ -21,4 +21,51 @@ Console.ReadLine();
 
 void SetZeroes(int[][] matrix)
 {
+    int indexCol = 0;
+    int indexRow = 0;
+
+    while (indexCol < matrix[0].Length || indexCol < matrix.Length)
+    {
+        if (indexRow < matrix.Length)
+        {
+            for (int col = indexCol; col < matrix[0].Length; col++)
+            {
+                if (matrix[indexRow][col] == 0)
+                {
+                    SettingTheLineToZero(matrix, col, indexRow, "col");
+                }
+            }
+        }
+        if (indexCol < matrix[0].Length)
+        {
+            for (int row = indexRow; row < matrix.Length; row++)
+            {
+                if (matrix[row][indexCol] == 0)
+                {
+                    SettingTheLineToZero(matrix, indexCol, row, "row");
+                }
+            }
+        }
+           
+        indexCol++;
+        indexRow++;
+    }
+}
+
+void SettingTheLineToZero(int[][] matrix,int indexCol, int indexRow, string direction)
+{
+    if (direction == "col")
+    {
+        for (int col = 0; col < matrix[0].Length; col++)
+        {
+            matrix[indexRow][col] = 0;
+        }
+    }
+    else if (direction == "row")
+    {
+        for (int row = 0; row < matrix.Length; row++)
+        {
+            matrix[row][indexCol] = 0;
+        }
+    }
 }
