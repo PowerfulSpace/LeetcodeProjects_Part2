34c8cbbbbfab2217facd29a70b733d4506ee231e
Alexander <sany199574@gmail.com>
2024-07-27 00:00:26 +0300

    Добавлен алгоритм

---
+++ project_76_Minimum Window Substring_2/Program.cs
@@ -0,0 +1,84 @@
+﻿
+using System.Text;
+
+string s1 = "ADOBECODEBANC", t1 = "ABC";
+string s2 = "a", t2 = "a";
+string s3 = "aa", t3 = "aa";
+
+
+Console.WriteLine(MinWindow(s1, t1));
+Console.WriteLine(MinWindow(s2, t2));
+Console.WriteLine(MinWindow(s3, t3));
+
+
+Console.ReadLine();
+
+
+string MinWindow(string s, string t)
+{
+    if (s.Length < t.Length) { return ""; }
+
+    Dictionary<char, int> demanding = new Dictionary<char, int>();
+
+    Rewrite(demanding, t);
+
+    int length = t.Length;
+
+    int minCount = int.MaxValue;
+    int count = 0;
+
+    int head = 0;
+    int index = 0;
+
+    int startOfLine = 0;
+    int endOfLine = 0;
+
+    while (index < s.Length)
+    {
+        if (demanding.ContainsKey(s[index]) && demanding[s[index]] != 0)
+        {
+            if (length == t.Length) { head = index; }
+
+            demanding[s[index]]--;
+            length--;
+        }
+
+        if (length == 0)
+        {
+            count = index - head;
+            if (minCount > count)
+            {
+                minCount = count;
+
+                startOfLine = head;
+                endOfLine = count + 1;
+            }
+
+            length = t.Length;
+            Rewrite(demanding, t);
+
+            head++;
+            index = head;
+            continue;
+        }
+
+        index++;
+    }
+
+    return s.Substring(startOfLine, endOfLine);
+}
+
+void Rewrite(Dictionary<char, int> demanding, string t)
+{
+    for (int i = 0; i < t.Length; i++)
+    {
+        if (demanding.ContainsKey(t[i]))
+        {
+            demanding[t[i]]++;
+        }
+        else
+        {
+            demanding.Add(t[i], 1);
+        }
+    }
+}
---
+++ project_76_Minimum Window Substring_3/Program.cs
@@ -0,0 +1,47 @@
+﻿
+string s1 = "ADOBECODEBANC", t1 = "ABC";
+string s2 = "a", t2 = "a";
+string s3 = "aa", t3 = "aa";
+
+
+Console.WriteLine(MinWindow(s1, t1));
+Console.WriteLine(MinWindow(s2, t2));
+Console.WriteLine(MinWindow(s3, t3));
+
+
+Console.ReadLine();
+
+
+string MinWindow(string s, string t)
+{
+    int[] need = new int[128];
+    int[] window = new int[128];
+    foreach (var c in t)
+    {
+        ++need[c];
+    }
+    int cnt = 0, j = 0, k = -1, mi = 1 << 30;
+    for (int i = 0; i < s.Length; ++i)
+    {
+        ++window[s[i]];
+        if (need[s[i]] >= window[s[i]])
+        {
+            ++cnt;
+        }
+        while (cnt == t.Length)
+        {
+            if (i - j + 1 < mi)
+            {
+                mi = i - j + 1;
+                k = j;
+            }
+            if (need[s[j]] >= window[s[j]])
+            {
+                --cnt;
+            }
+            --window[s[j++]];
+        }
+    }
+    return k < 0 ? "" : s.Substring(k, mi);
+}
+
