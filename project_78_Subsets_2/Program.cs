a9c32ef780641a163f5b25fac5024d970f043974
Alexander <sany199574@gmail.com>
2024-07-28 06:14:09 +0300

    Добавлена задача

---
+++ project_78_Subsets_2/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+int[] nums1 = { 1, 2, 3 };
+int[] nums2 = { 0 };
+
+Subsets(nums1);
+Subsets(nums2);
+
+
+Console.ReadLine();
+
+
+
+IList<IList<int>> Subsets(int[] nums)
+{
+    List<IList<int>> result = new List<IList<int>>();
+    result.Add(new List<int>());
+    int left = 0;
+    int rigth = 1;
+
+    int count = 1;
+    int index = 0;
+
+
+    while (count <= nums.Length)
+    {
+        List<int> list;
+
+        while (rigth <= nums.Length)
+        {
+            list = new List<int>();
+            for (int i = left; i < rigth; i++)
+            {
+                list.Add(nums[i]);
+            }
+            result.Add(list);
+            left++;
+            rigth++;
+        }
+
+        count++;
+        left = 0;
+        rigth = count;
+    }
+
+
+    return result;
+}
+
