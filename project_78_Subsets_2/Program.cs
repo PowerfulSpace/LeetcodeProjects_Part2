c6ff2a3ecc8d579d36460531e1fd07dc317c8a8d
Alexander <sany199574@gmail.com>
2024-07-29 01:02:02 +0300

    Изменён алгоритм

--- project_78_Subsets_2/Program.cs
+++ project_78_Subsets_2/Program.cs
@@ -14,8 +14,25 @@ Console.ReadLine();
 IList<IList<int>> Subsets(int[] nums)
 {
     List<IList<int>> result = new List<IList<int>>();
-    
+
+    SubArrays(nums, 0,new List<int>(), result);
 
     return result;
 }
 
+void SubArrays(int[] nums, int index, List<int> list, List<IList<int>> result)
+{
+    result.Add(new List<int>(list));
+
+    if (index == nums.Length)
+    {
+        return;
+    }
+    
+    for (int i = index; i < nums.Length; i++)
+    {
+        list.Add(nums[i]);
+        SubArrays(nums,i + 1,list,result);
+        list.RemoveAt(list.Count - 1);
+    }
+}
