5c3c2722ebe1370cdf2f1b002aaf2e0619099c25
Alexander <sany199574@gmail.com>
2024-07-29 01:22:01 +0300

    Добавлен алгоритм

---
+++ project_78_Subsets_3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] nums1 = { 1, 2, 3 };
+int[] nums2 = { 0 };
+
+Subsets(nums1);
+Subsets(nums2);
+
+
+Console.ReadLine();
+
+
+
+IList<IList<int>> Subsets(int[] nums)
+{
+    List<IList<int>> subsets = new List<IList<int>>();
+    subsets.Add(new List<int>());
+    foreach (int x in nums)
+    {
+        var tmp = subsets;
+        foreach (List<int> list in subsets.ToArray())
+        {
+            List<int> tmpList = new(list);
+            tmpList.Add(x);
+            tmp.Add(tmpList);
+        }
+        subsets = tmp;
+    }
+    return subsets;
+}
