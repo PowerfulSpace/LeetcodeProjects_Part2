7db18d4ce7af972a9f2f5fd0b90a83c3d7c7be78
Alexander <sany199574@gmail.com>
2024-07-29 00:03:20 +0300

    Добавлен алгоритм

--- project_78_Subsets_2/Program.cs
+++ project_78_Subsets_2/Program.cs
@@ -14,35 +14,7 @@ Console.ReadLine();
 IList<IList<int>> Subsets(int[] nums)
 {
     List<IList<int>> result = new List<IList<int>>();
-    result.Add(new List<int>());
-    int left = 0;
-    int rigth = 1;
-
-    int count = 1;
-    int index = 0;
-
-
-    while (count <= nums.Length)
-    {
-        List<int> list;
-
-        while (rigth <= nums.Length)
-        {
-            list = new List<int>();
-            for (int i = left; i < rigth; i++)
-            {
-                list.Add(nums[i]);
-            }
-            result.Add(list);
-            left++;
-            rigth++;
-        }
-
-        count++;
-        left = 0;
-        rigth = count;
-    }
-
+    
 
     return result;
 }
