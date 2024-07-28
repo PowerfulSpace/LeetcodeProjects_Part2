f5174c408c54fc93f6a20f98867463fb838f09cf
Alexander <sany199574@gmail.com>
2024-07-29 01:37:20 +0300

    Добавлены тесты

---
+++ project_79_Word Search/Program.cs
@@ -0,0 +1,38 @@
+﻿
+char[][] board1 = new char[3][]
+{
+    new char[4] { 'A', 'B', 'C', 'E' },
+    new char[4] { 'S', 'F', 'C', 'S' },
+    new char[4] { 'A', 'D', 'E', 'E' }
+};
+string word1 = "ABCCED";
+
+char[][] board2 = new char[3][]
+{
+    new char[4] {'A', 'B', 'C', 'E'},
+    new char[4] {'S', 'F', 'C', 'S'},
+    new char[4] { 'A', 'D', 'E', 'E'}
+};
+string word2 = "SEE";
+
+char[][] board3 = new char[3][]
+{
+    new char[4] {'A', 'B', 'C', 'E'},
+    new char[4] {'S', 'F', 'C', 'S'},
+    new char[4] {'A', 'D', 'E', 'E'}
+};
+string word3 = "ABCB";
+
+
+Console.WriteLine(Exist(board1, word1));
+Console.WriteLine(Exist(board2, word2));
+Console.WriteLine(Exist(board3, word3));
+
+
+Console.ReadLine();
+
+
+bool Exist(char[][] board, string word)
+{
+    return true;
+}
