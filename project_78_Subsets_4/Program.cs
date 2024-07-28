f69eb47a7f5bb80d6c894e00c190486ef36f63b1
Alexander <sany199574@gmail.com>
2024-07-29 01:26:03 +0300

    Добавлен алгоритм

---
+++ project_78_Subsets_4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+int[] nums1 = { 1, 2, 3 };
+int[] nums2 = { 0 };
+
+Subsets(nums1);
+Subsets(nums2);
+
+
+Console.ReadLine();
+
+
+
+IList<IList<int>> Subsets(int[] nums)
+{
+    var ret = new List<IList<int>>();
+    GenerateSubsets(nums, 0, new List<int>(), ret);
+    return ret;
+}
+void GenerateSubsets(int[] num, int pos, List<int> temp, List<IList<int>> ret)
+{
+    ret.Add(new List<int>(temp));
+    for (int i = pos; i < num.Length; i++)
+    {
+        temp.Add(num[i]);
+        GenerateSubsets(num, i + 1, temp, ret);
+        temp.RemoveAt(temp.Count - 1);
+    }
+}
