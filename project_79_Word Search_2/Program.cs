dba6b8155a6891dfa77fae3affb8aa5d53303b5e
Alexander <sany199574@gmail.com>
2024-07-30 00:54:29 +0300

    Добавлен алгоритм

--- project_79_Word Search_2/Program.cs
+++ project_79_Word Search_2/Program.cs
@@ -25,7 +25,7 @@ char[][] board3 = new char[3][]
 string word3 = "ABCB";
 
 
-//Console.WriteLine(Exist(board1, word1));
+Console.WriteLine(Exist(board1, word1));
 Console.WriteLine(Exist(board2, word2));
 Console.WriteLine(Exist(board3, word3));
 
@@ -36,110 +36,45 @@ Console.ReadLine();
 //Ошибка алгоритма, в добавлении элемента с массива который уже используетися.
 //Добавить ограничения в ввиде массива с указателями
 
-
 bool Exist(char[][] board, string word)
 {
-    List<char> list = new List<char>();
-    List<(int, int)> start = FindTheFirstLitter(board, word[0], list);
-    bool[,] block = new bool[board.Length, board[0].Length];
-
-    bool result = false;
-
-    int index = 0;
-    foreach ((int, int) c in start)
+    for (int i = 0; i < board.Length; i++)
     {
-        list.Add(word[0]);
-        block[start[index].Item1, start[index].Item2] = true;
+        for (int j = 0; j < board[0].Length; j++)
+        {
 
-        result = WordSerach(board, 1, c.Item1, c.Item2, list, word, result, block);
+            if (board[i][j] == word[0])
+            {
+                char[] wordArray = word.ToCharArray();
+                bool flag = helperFunc(board, wordArray, i, j, 0);
+                if (flag == true)
+                {
+                    return true;
+                }
+            }
 
-        if (result == true) break;
-
-        list.RemoveAt(list.Count - 1);
-        block[start[index].Item1, start[index].Item2] = false;
-
-        index++;
+        }
     }
-
-
-    return result;
+    return false;
 }
 
-
-bool WordSerach(char[][] board, int index, int y, int x, List<char> litters, string word, bool result, bool[,] block)
+bool helperFunc(char[][] board, char[] word, int i, int j, int cur)
 {
-    if (litters.Count == word.Length)
+    if (cur == word.Length)
     {
         return true;
     }
-
-    if (x < 0 || x > board[0].Length - 1 || y < 0 || y > board.Length - 1) { return false; }
-
-    if ((litters.Count == 1 ? block[y, x] == true : block[y, x] == false) && x - 1 >= 0 && word[index] == board[y][x - 1])
+    if (i < 0 || i >= board.Length || j < 0 || j >= board[0].Length || board[i][j] != word[cur])
     {
-        litters.Add(word[index]);
-        block[y, x] = true;
-        result = WordSerach(board, index + 1, y, x - 1, litters, word, result, block);
-
-        if (result) { return result; }
-
-        block[y, x] = false;
-        litters.RemoveAt(litters.Count - 1);
+        return false;
     }
-    if ((litters.Count == 1 ? block[y, x] == true : block[y, x] == false) && y - 1 >= 0 && word[index] == board[y - 1][x])
-    {
-        litters.Add(word[index]);
-        block[y, x] = true;
+    char temp = board[i][j];
+    board[i][j] = '$';
+    bool a = helperFunc(board, word, i + 1, j, cur + 1);
+    bool b = helperFunc(board, word, i - 1, j, cur + 1);
+    bool c = helperFunc(board, word, i, j + 1, cur + 1);
+    bool d = helperFunc(board, word, i, j - 1, cur + 1);
+    board[i][j] = temp;
+    return a | b | c | d;
 
-        result = WordSerach(board, index + 1, y - 1, x, litters, word, result, block);
-
-        if (result) { return result; }
-
-        block[y, x] = false;
-        litters.RemoveAt(litters.Count - 1);
-    }
-    if ((litters.Count == 1 ? block[y, x] == true : block[y, x] == false) && x + 1 < board[0].Length && word[index] == board[y][x + 1])
-    {
-        litters.Add(word[index]);
-        block[y, x] = true;
-
-        result = WordSerach(board, index + 1, y, x + 1, litters, word, result, block);
-
-        if (result) { return result; }
-
-        block[y, x] = false;
-        litters.RemoveAt(litters.Count - 1);
-    }
-    if ((litters.Count == 1 ? block[y, x] == true : block[y, x] == false) && y + 1 < board.Length && word[index] == board[y + 1][x])
-    {
-        litters.Add(word[index]);
-        block[y, x] = true;
-
-        result = WordSerach(board, index + 1, y + 1, x, litters, word, result, block);
-
-        if (result) { return result; }
-
-        block[y, x] = false;
-        litters.RemoveAt(litters.Count - 1);
-    }
-    return result;
-}
-
-
-
-List<(int, int)> FindTheFirstLitter(char[][] board, char litter, List<char> result)
-{
-    List<(int, int)> list = new List<(int, int)>();
-
-    for (int i = 0; i < board.Length; i++)
-    {
-        for (int j = 0; j < board[i].Length; j++)
-        {
-            if (board[i][j] == litter)
-            {
-                list.Add((i, j));
-            }
-        }
-    }
-    return list;
 }
---
+++ project_79_Word Search_3/Program.cs
@@ -0,0 +1,165 @@
+﻿
+
+char[][] board1 = new char[3][]
+{
+    new char[4] { 'A', 'B', 'C', 'E' },
+    new char[4] { 'S', 'F', 'C', 'S' },
+    new char[4] { 'A', 'D', 'E', 'E' }
+};
+string word1 = "ABCCED";
+
+char[][] board2 = new char[3][]
+{
+    new char[4] {'A', 'B', 'C', 'E'},
+    new char[4] {'S', 'F', 'C', 'S'},
+    new char[4] { 'A', 'D', 'E', 'E'}
+};
+string word2 = "SEE";
+
+char[][] board3 = new char[3][]
+{
+    new char[4] {'A', 'B', 'C', 'E'},
+    new char[4] {'S', 'F', 'C', 'S'},
+    new char[4] {'A', 'D', 'E', 'E'}
+};
+string word3 = "ABCB";
+
+
+Console.WriteLine(Exist(board1, word1));
+Console.WriteLine(Exist(board2, word2));
+Console.WriteLine(Exist(board3, word3));
+
+
+Console.ReadLine();
+
+
+//Ошибка алгоритма, в добавлении элемента с массива который уже используетися.
+//Добавить ограничения в ввиде массива с указателями
+
+bool Exist(char[][] board, string word)
+{
+    // optimization
+    Dictionary<char, int> boardLetters = GetBoardLetterCount(board);
+    Dictionary<char, int> wordLetters = GetWordLetterCount(word);
+
+    // compare letter count, unique chars, etc. 
+    if (!LetterCheck(boardLetters, wordLetters))
+        return false;
+
+    // reverse word if last letter occurs less than first
+    word = GetOptimizedWord(word, wordLetters);
+
+    // optimization end
+    HashSet<(int row, int col)> visited = new HashSet<(int, int)>();
+
+    // run dfs 
+    for (int row = 0; row < board.Length; row++)
+    {
+        for (int col = 0; col < board[row].Length; col++)
+        {
+            if (DFS(board, visited, word, row, col, 0))
+                return true;
+        }
+    }
+    return false;
+}
+
+bool LetterCheck(Dictionary<char, int> boardLetters, Dictionary<char, int> wordLetters)
+{
+    // return false if board does not have same amount of unique chars as word
+    if (boardLetters.Keys.Count < wordLetters.Keys.Count)
+        return false;
+
+    // otherwise, go through each unique letter in word and check whether board possibly has it
+    foreach (var key in wordLetters.Keys)
+    {
+        // board does not have letter
+        if (!boardLetters.ContainsKey(key))
+            return false;
+
+        // board letter count is less than word letter count
+        if (boardLetters[key] < wordLetters[key])
+            return false;
+    }
+    return true;
+}
+string GetOptimizedWord(string word, Dictionary<char, int> wordLetters)
+{
+    if (wordLetters[word[0]] > wordLetters[word[word.Length - 1]])
+    {
+        return Reverse(word);
+    }
+    return word;
+}
+string Reverse(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+Dictionary<char, int> GetWordLetterCount(string word)
+{
+    Dictionary<char, int> letterCnt = new Dictionary<char, int>();
+    foreach (char c in word)
+    {
+        if (letterCnt.ContainsKey(c))
+        {
+            letterCnt[c]++;
+        }
+        else
+        {
+            letterCnt[c] = 1;
+        }
+    }
+    return letterCnt;
+}
+Dictionary<char, int> GetBoardLetterCount(char[][] board)
+{
+    Dictionary<char, int> letterCnt = new Dictionary<char, int>();
+    for (int row = 0; row < board.Length; row++)
+    {
+        for (int col = 0; col < board[row].Length; col++)
+        {
+            char c = board[row][col];
+            if (letterCnt.ContainsKey(c))
+            {
+                letterCnt[c]++;
+            }
+            else
+            {
+                letterCnt[c] = 1;
+            }
+        }
+    }
+    return letterCnt;
+}
+
+// call stack can be at most length of word
+// O(n * m * dfs) - dimensions of matrix * dfs algo
+// dfs algo - call stack is at most the size of the word
+// we are calling 4 times, 4 ^ n where n is the length of the word
+
+bool DFS(char[][] board, HashSet<(int, int)> visited, string word, int row, int col, int i)
+{
+    // base case, we matched all characters
+    if (i == word.Length)
+        return true;
+
+    if (
+        (row < 0 || row >= board.Length) ||
+        (col < 0 || col >= board[row].Length) ||
+        visited.Contains((row, col)) ||
+        board[row][col] != word[i]
+    )
+        return false;
+
+    visited.Add((row, col));
+    var result = (
+        DFS(board, visited, word, row + 1, col, i + 1) ||
+        DFS(board, visited, word, row - 1, col, i + 1) ||
+        DFS(board, visited, word, row, col + 1, i + 1) ||
+        DFS(board, visited, word, row, col - 1, i + 1)
+    );
+    visited.Remove((row, col));
+    return result;
+}
