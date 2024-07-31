a6e52e5580f87a7f63d3a8be4634ac3e6e322aaa
Alexander <sany199574@gmail.com>
2024-07-31 04:05:43 +0300

    Добавлен алгоритм

--- project_80_Remove Duplicates from Sorted Array II/Program.cs
+++ project_80_Remove Duplicates from Sorted Array II/Program.cs
@@ -3,7 +3,7 @@
 int[] nums1 = { 1, 1, 1, 2, 2, 3 };
 int[] nums2 = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
 
-Console.WriteLine(RemoveDuplicates(nums1));
+//Console.WriteLine(RemoveDuplicates(nums1));
 Console.WriteLine(RemoveDuplicates(nums2));
 
 Console.ReadLine();
@@ -12,6 +12,33 @@ Console.ReadLine();
 
 int RemoveDuplicates(int[] nums)
 {
+	Dictionary<int,int> dict = new Dictionary<int,int>();
+	int count = 0;
+
+	for (int i = 0; i < nums.Length; i++)
+	{
+        if (dict.ContainsKey(nums[i]))
+        {
+            if (dict[nums[i]] < 2)
+            {
+                dict[nums[i]]++;
+            }
+            else
+            {
+                if (i + 1 < nums.Length - count)
+                {
+                    nums[i] = nums[i + 1 + count];
+                    count++;
+                    i--;
+                }
+            }
+        }
+        else
+        {
+            dict[nums[i]] = 1;
+        }
+    }
+
     return 1;
 }
 
